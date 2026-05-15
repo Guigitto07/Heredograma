4120ae9835afe882f76c3c85a405da08ca4d0386
Guigitto07 <guilhermefilipak08@gmail.com>
2026-05-15 14:12:18 -0300

    Primeira lei de Mendel

---
+++ heredograma.py
@@ -0,0 +1,58 @@
+Geracoes = int(input("Quantidade de geraçoes :"))
+pessoas = [
+
+]
+quantidadeDePessoas = int(input('Quantidade de pessaos'))
+
+
+
+
+
+
+
+
+def primeiraLeideMendel(x,y):
+    Contador = 0
+    #mae/////////////////////////////////////
+    genotipoMae = x
+    a = 0
+    b = 0
+    for i in genotipoMae:
+        Contador = Contador + 1
+        if i == "A":
+            if Contador == 1:
+                a = 1 
+            elif Contador == 2:
+                b = 1
+    #Pai////////////////////////////////////////
+    genotipoPai = y
+    c = 0
+    d = 0
+    for z in genotipoPai:
+        Contador = Contador + 1
+        if z == "A":
+            if Contador == 3:
+                c = 1 
+            elif Contador == 4:
+                d = 1
+    # mendel 
+    soma = a + b + c + d
+    print()
+    if soma == 2:
+        if ((c == 0) and (d == 0)) or ((a == 0) and (b==0)):
+            print("100% hterozigoto")
+        else:
+            print("50% Heterozigoto\n25% Homozigoto dominante\n25% Homozigoto recessivo")
+    elif soma == 3:
+        print("50% hterozigoto\n50% Homozigoto dominante")
+    elif soma == 4:
+        print("100% Homozigoto dominante")
+    elif soma == 1:
+        print("50% hterozigoto\n50% Homozigoto recessivo")
+    elif soma == 0:
+        print("100% Homozigoto recessivo")
+
+    
+primeiraLeideMendel("Aa","Aa")
+
+
